b208329d8c21dc78d1e4f5b4f0a5feef3a4eb01a
Enrique Navarro <enavarro@iicv.es>
2026-04-18 08:28:07 +0200

    fix: usar DATABASE_URL en Railway

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_USER = "postgres"
-DB_PASSWORD = "Ena12345="
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "tutor_db"
+# Obtener la URL desde Railway
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = (
-    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+# Ajuste necesario para Railway (postgres -> postgresql)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
